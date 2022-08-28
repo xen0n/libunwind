xen0n/libunwind

commit c5f1d12c77dea6a60740730c675fc56b3c52b86a

Add port for Linux on LoongArch
This commit adds support for Linux on LoongArch. Only 64-bit is supported
at the moment.

The changes were tested on a LoongArch architecture Loongson-3A5000
processor (LoongArch is a new RISC ISA, which is a bit like MIPS or
RISC-V.).

Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
Signed-off-by: Qing Zhang <zhangqing@loongson.cn>
Signed-off-by: Jinyang He <hejinyang@loongson.cn>
Signed-off-by: Youling Tang <tangyouling@loongson.cn>

src/coredump/_UCD_access_reg_linux.c
@@ -65,6 +65,43 @@ _UCD_access_reg (unw_addr_space_t as,
     regnum = 0;
   else if (regnum > UNW_RISCV_X31)
     goto badreg;
+#elif defined(UNW_TARGET_LOONGARCH64)
+# include <asm/reg.h>
+
+  static const uint8_t remap_regs[] =
+    {
+      [UNW_LOONGARCH64_R0]  = LOONGARCH64_EF_R0,
+      [UNW_LOONGARCH64_R1]  = LOONGARCH64_EF_R1,
+      [UNW_LOONGARCH64_R2]  = LOONGARCH64_EF_R2,
+      [UNW_LOONGARCH64_R3]  = LOONGARCH64_EF_R3,
+      [UNW_LOONGARCH64_R4]  = LOONGARCH64_EF_R4,
+      [UNW_LOONGARCH64_R5]  = LOONGARCH64_EF_R5,
+      [UNW_LOONGARCH64_R6]  = LOONGARCH64_EF_R6,
+      [UNW_LOONGARCH64_R7]  = LOONGARCH64_EF_R7,
+      [UNW_LOONGARCH64_R8]  = LOONGARCH64_EF_R8,
+      [UNW_LOONGARCH64_R9]  = LOONGARCH64_EF_R9,
+      [UNW_LOONGARCH64_R10] = LOONGARCH64_EF_R10,
+      [UNW_LOONGARCH64_R11] = LOONGARCH64_EF_R11,
+      [UNW_LOONGARCH64_R12] = LOONGARCH64_EF_R12,
+      [UNW_LOONGARCH64_R13] = LOONGARCH64_EF_R13,
+      [UNW_LOONGARCH64_R14] = LOONGARCH64_EF_R14,
+      [UNW_LOONGARCH64_R15] = LOONGARCH64_EF_R15,
+      [UNW_LOONGARCH64_R16] = LOONGARCH64_EF_R16,
+      [UNW_LOONGARCH64_R17] = LOONGARCH64_EF_R17,
+      [UNW_LOONGARCH64_R18] = LOONGARCH64_EF_R18,
+      [UNW_LOONGARCH64_R19] = LOONGARCH64_EF_R19,
+      [UNW_LOONGARCH64_R20] = LOONGARCH64_EF_R20,
+      [UNW_LOONGARCH64_R21] = LOONGARCH64_EF_R21,
+      [UNW_LOONGARCH64_R22] = LOONGARCH64_EF_R22,
+      [UNW_LOONGARCH64_R23] = LOONGARCH64_EF_R23,
+      [UNW_LOONGARCH64_R24] = LOONGARCH64_EF_R24,
+      [UNW_LOONGARCH64_R25] = LOONGARCH64_EF_R25,
+      [UNW_LOONGARCH64_R28] = LOONGARCH64_EF_R28,
+      [UNW_LOONGARCH64_R29] = LOONGARCH64_EF_R29,
+      [UNW_LOONGARCH64_R30] = LOONGARCH64_EF_R30,
+      [UNW_LOONGARCH64_R31] = LOONGARCH64_EF_R31,
+      [UNW_LOONGARCH64_PC]  = LOONGARCH64_EF_CSR_EPC,
+    };
 #else
 #if defined(UNW_TARGET_MIPS)
   static const uint8_t remap_regs[] =

src/ptrace/_UPT_reg_offset.c
@@ -754,6 +754,42 @@ const int _UPT_reg_offset[UNW_REG_LAST + 1] =
     [UNW_RISCV_X29] = RISCV_REG_OFFSET(29),
     [UNW_RISCV_X30] = RISCV_REG_OFFSET(30),
     [UNW_RISCV_X31] = RISCV_REG_OFFSET(31),
+#elif defined(UNW_TARGET_LOONGARCH64)
+# include <asm/reg.h>
+
+    [UNW_LOONGARCH64_R0]  = LOONGARCH64_EF_R0,
+    [UNW_LOONGARCH64_R1]  = LOONGARCH64_EF_R1,
+    [UNW_LOONGARCH64_R2]  = LOONGARCH64_EF_R2,
+    [UNW_LOONGARCH64_R3]  = LOONGARCH64_EF_R3,
+    [UNW_LOONGARCH64_R4]  = LOONGARCH64_EF_R4,
+    [UNW_LOONGARCH64_R5]  = LOONGARCH64_EF_R5,
+    [UNW_LOONGARCH64_R6]  = LOONGARCH64_EF_R6,
+    [UNW_LOONGARCH64_R7]  = LOONGARCH64_EF_R7,
+    [UNW_LOONGARCH64_R8]  = LOONGARCH64_EF_R8,
+    [UNW_LOONGARCH64_R9]  = LOONGARCH64_EF_R9,
+    [UNW_LOONGARCH64_R10] = LOONGARCH64_EF_R10,
+    [UNW_LOONGARCH64_R11] = LOONGARCH64_EF_R11,
+    [UNW_LOONGARCH64_R12] = LOONGARCH64_EF_R12,
+    [UNW_LOONGARCH64_R13] = LOONGARCH64_EF_R13,
+    [UNW_LOONGARCH64_R14] = LOONGARCH64_EF_R14,
+    [UNW_LOONGARCH64_R15] = LOONGARCH64_EF_R15,
+    [UNW_LOONGARCH64_R16] = LOONGARCH64_EF_R16,
+    [UNW_LOONGARCH64_R17] = LOONGARCH64_EF_R17,
+    [UNW_LOONGARCH64_R18] = LOONGARCH64_EF_R18,
+    [UNW_LOONGARCH64_R19] = LOONGARCH64_EF_R19,
+    [UNW_LOONGARCH64_R20] = LOONGARCH64_EF_R20,
+    [UNW_LOONGARCH64_R21] = LOONGARCH64_EF_R21,
+    [UNW_LOONGARCH64_R22] = LOONGARCH64_EF_R22,
+    [UNW_LOONGARCH64_R23] = LOONGARCH64_EF_R23,
+    [UNW_LOONGARCH64_R24] = LOONGARCH64_EF_R24,
+    [UNW_LOONGARCH64_R25] = LOONGARCH64_EF_R25,
+    [UNW_LOONGARCH64_R26] = LOONGARCH64_EF_R26,
+    [UNW_LOONGARCH64_R27] = LOONGARCH64_EF_R27,
+    [UNW_LOONGARCH64_R28] = LOONGARCH64_EF_R28,
+    [UNW_LOONGARCH64_R29] = LOONGARCH64_EF_R29,
+    [UNW_LOONGARCH64_R30] = LOONGARCH64_EF_R30,
+    [UNW_LOONGARCH64_R31] = LOONGARCH64_EF_R31,
+    [UNW_LOONGARCH64_PC]  = LOONGARCH64_EF_CSR_EPC
 #else
 # error Fix me.
 #endif

Makefile.am
@@ -47,6 +47,9 @@ endif
 if ARCH_RISCV
 include_HEADERS += include/libunwind-riscv.h
 endif
+if ARCH_LOONGARCH64
+include_HEADERS += include/libunwind-loongarch64.h
+endif
 
 if !REMOTE_ONLY
 include_HEADERS += include/libunwind.h
@@ -97,6 +100,9 @@ noinst_HEADERS = include/dwarf.h include/dwarf_i.h include/dwarf-eh.h	\
 	include/tdep-s390x/jmpbuf.h include/tdep-s390x/libunwind_i.h	\
 	include/tdep-riscv/dwarf-config.h				\
 	include/tdep-riscv/jmpbuf.h include/tdep-riscv/libunwind_i.h	\
+	include/tdep-loongarch64/dwarf-config.h				\
+	include/tdep-loongarch64/jmpbuf.h				\
+	include/tdep-loongarch64/libunwind_i.h				\
 	include/tdep/libunwind_i.h					\
 	include/tdep/jmpbuf.h include/tdep/dwarf-config.h
 

README
@@ -18,6 +18,7 @@ This library supports several architecture/operating-system combinations:
 | Linux   | Tilegx       | 64-bit mode only |
 | Linux   | MIPS         | Newly added |
 | Linux   | RISC-V       | 64-bit only |
+| Linux   | LoongArch    | 64-bit only |
 | HP-UX   | IA-64        | Mostly works, but known to have serious limitations |
 | FreeBSD | x86-64       | ✓      |
 | FreeBSD | x86          | ✓      |
@@ -41,6 +42,7 @@ such dependencies
 |    arm       |     p      |            |
 |    hppa      |     p      |      p     |
 |    ia64      |     p      |      r     |
+|    loongarch |     p      |            |
 |    mips      |     p      |            |
 |    ppc32     |     r      |            |
 |    ppc64     |     r      |      r     |

configure.ac
@@ -98,6 +98,7 @@ AC_DEFUN([SET_ARCH],[
         [amd64],[$2=x86_64],
         [tile*],[$2=tilegx],
         [riscv*],[$2=riscv],
+        [loongarch64*],[$2=loongarch64],
         [$2=$1])
 ]) dnl SET_ARCH
 
@@ -120,7 +121,7 @@ esac
 
 AC_ARG_ENABLE(coredump,
 	AS_HELP_STRING([--enable-coredump],[building libunwind-coredump library]),,
-        [AS_CASE([$host_arch], [aarch64*|arm*|mips*|sh*|x86*|tile*|riscv*], [enable_coredump=yes], [enable_coredump=no])]
+        [AS_CASE([$host_arch], [aarch64*|arm*|mips*|sh*|x86*|tile*|riscv*|loongarch64], [enable_coredump=yes], [enable_coredump=no])]
 )
 
 AC_MSG_CHECKING([if we should build libunwind-coredump])
@@ -189,6 +190,7 @@ AM_CONDITIONAL(ARCH_SH, test x$target_arch = xsh)
 AM_CONDITIONAL(ARCH_TILEGX, test x$target_arch = xtilegx)
 AM_CONDITIONAL(ARCH_S390X, test x$target_arch = xs390x)
 AM_CONDITIONAL(ARCH_RISCV, test x$target_arch = xriscv)
+AM_CONDITIONAL(ARCH_LOONGARCH64, test x$target_arch = xloongarch64)
 AM_CONDITIONAL(OS_LINUX, expr x$target_os : xlinux >/dev/null)
 AM_CONDITIONAL(OS_HPUX, expr x$target_os : xhpux >/dev/null)
 AM_CONDITIONAL(OS_FREEBSD, expr x$target_os : xfreebsd >/dev/null)
@@ -199,7 +201,7 @@ AC_MSG_CHECKING([for ELF helper width])
 case "${target_arch}" in
 (arm|hppa|ppc32|x86|sh) use_elf32=yes; AC_MSG_RESULT([32]);;
 (aarch64|ia64|ppc64|x86_64|s390x|tilegx)  use_elf64=yes; AC_MSG_RESULT([64]);;
-(mips|riscv)                 use_elfxx=yes; AC_MSG_RESULT([xx]);;
+(mips|riscv|loongarch64)                  use_elfxx=yes; AC_MSG_RESULT([xx]);;
 *)                     AC_MSG_ERROR([Unknown ELF target: ${target_arch}])
 esac
 AM_CONDITIONAL(USE_ELF32, [test x$use_elf32 = xyes])
@@ -246,6 +248,7 @@ case $target_arch in
   mips*) enable_cxx_exceptions=no;;
   tile*) enable_cxx_exceptions=no;;
   s390x*) enable_cxx_exceptions=no;;
+  loongarch*) enable_cxx_exceptions=no;;
   *) enable_cxx_exceptions=yes;;
 esac
 ])

include/libunwind-loongarch64.h
@@ -1,0 +1,139 @@
+/* libunwind - a platform-independent unwind library
+   Copyright (C) 2001-2004 Hewlett-Packard Co
+        Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
+   Copyright (C) 2021 Loongson Technology Corporation Limited
+
+This file is part of libunwind.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+
+#ifndef LIBUNWIND_H
+#define LIBUNWIND_H
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+#include <inttypes.h>
+#include <ucontext.h>
+
+#define UNW_TARGET      loongarch64
+#define UNW_TARGET_LOONGARCH64 1
+
+#define _U_TDEP_QP_TRUE 0       /* see libunwind-dynamic.h  */
+
+/* This needs to be big enough to accommodate "struct cursor", while
+   leaving some slack for future expansion.  Changing this value will
+   require recompiling all users of this library.  Stack allocation is
+   relatively cheap and unwind-state copying is relatively rare, so we
+   want to err on making it rather too big than too small.  */
+
+/* FIXME for LoongArch64. Too big?  What do other things use for similar tasks?  */
+#define UNW_TDEP_CURSOR_LEN     4096
+
+typedef uint64_t unw_word_t;
+typedef int64_t unw_sword_t;
+
+typedef long double unw_tdep_fpreg_t;
+
+typedef enum
+  {
+    UNW_LOONGARCH64_R0,
+    UNW_LOONGARCH64_R1,
+    UNW_LOONGARCH64_R2,
+    UNW_LOONGARCH64_R3,
+    UNW_LOONGARCH64_R4,
+    UNW_LOONGARCH64_R5,
+    UNW_LOONGARCH64_R6,
+    UNW_LOONGARCH64_R7,
+    UNW_LOONGARCH64_R8,
+    UNW_LOONGARCH64_R9,
+    UNW_LOONGARCH64_R10,
+    UNW_LOONGARCH64_R11,
+    UNW_LOONGARCH64_R12,
+    UNW_LOONGARCH64_R13,
+    UNW_LOONGARCH64_R14,
+    UNW_LOONGARCH64_R15,
+    UNW_LOONGARCH64_R16,
+    UNW_LOONGARCH64_R17,
+    UNW_LOONGARCH64_R18,
+    UNW_LOONGARCH64_R19,
+    UNW_LOONGARCH64_R20,
+    UNW_LOONGARCH64_R21,
+    UNW_LOONGARCH64_R22,
+    UNW_LOONGARCH64_R23,
+    UNW_LOONGARCH64_R24,
+    UNW_LOONGARCH64_R25,
+    UNW_LOONGARCH64_R26,
+    UNW_LOONGARCH64_R27,
+    UNW_LOONGARCH64_R28,
+    UNW_LOONGARCH64_R29,
+    UNW_LOONGARCH64_R30,
+    UNW_LOONGARCH64_R31,
+
+    UNW_LOONGARCH64_PC = 32,
+
+    /* FIXME: Other registers!  */
+
+    /* For LoongArch64, the CFA is the value of SP (r3) at the call site in the
+       previous frame.  */
+    UNW_LOONGARCH64_CFA,
+
+    UNW_TDEP_LAST_REG = UNW_LOONGARCH64_PC,
+
+    UNW_TDEP_IP = UNW_LOONGARCH64_R1,
+    UNW_TDEP_SP = UNW_LOONGARCH64_R3,
+    UNW_TDEP_EH = UNW_LOONGARCH64_R0   /* FIXME.  */
+  }
+loongarch64_regnum_t;
+
+#define UNW_TDEP_NUM_EH_REGS	4
+
+typedef struct unw_tdep_save_loc
+  {
+    /* Additional target-dependent info on a save location.  */
+    char unused;
+  }
+unw_tdep_save_loc_t;
+
+/* On LoongArch64, we can directly use ucontext_t as the unwind context. */
+typedef ucontext_t unw_tdep_context_t;
+
+typedef struct
+  {
+    /* no loongarch64-specific auxiliary proc-info */
+    char unused;
+  }
+unw_tdep_proc_info_t;
+
+#include "libunwind-dynamic.h"
+#include "libunwind-common.h"
+
+#define unw_tdep_getcontext		UNW_ARCH_OBJ(getcontext)
+extern int unw_tdep_getcontext (ucontext_t *uc);
+
+#define unw_tdep_is_fpreg		UNW_ARCH_OBJ(is_fpreg)
+extern int unw_tdep_is_fpreg (int);
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+
+#endif /* LIBUNWIND_H */

include/libunwind.h.in
@@ -29,6 +29,8 @@
 # include "libunwind-s390x.h"
 #elif defined __riscv || defined __riscv__
 # include "libunwind-riscv.h"
+#elif defined __loongarch64
+# include "libunwind-loongarch64.h"
 #else
 # error "Unsupported arch"
 #endif

include/tdep-loongarch64/dwarf-config.h
@@ -1,0 +1,59 @@
+/* libunwind - a platform-independent unwind library
+   Copyright (C) 2008 CodeSourcery
+   Copyright (C) 2012 Tommi Rantala <tt.rantala@gmail.com>
+   Copyright (C) 2021 Loongson Technology Corporation Limited
+
+This file is part of libunwind.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+
+#ifndef dwarf_config_h
+#define dwarf_config_h
+
+/* This is FIRST_PSEUDO_REGISTER in GCC, since DWARF_FRAME_REGISTERS is not
+   explicitly defined.
+   Number of hardware registers.  We have:
+     - 32 integer registers
+     - 32 floating point registers
+     - 8 condition code registers
+     - 2 fake registers:
+        - ARG_POINTER_REGNUM
+        - FRAME_POINTER_REGNUM  */
+#define DWARF_NUM_PRESERVED_REGS	74
+
+#define dwarf_to_unw_regnum(reg) (((reg) < 32) ? (reg) : 0)
+
+/* Not big-endian.  */
+#define dwarf_is_big_endian(addr_space)	0
+
+/* Convert a pointer to a dwarf_cursor structure to a pointer to
+   unw_cursor_t.  */
+#define dwarf_to_cursor(c)      ((unw_cursor_t *) (c))
+
+typedef struct dwarf_loc
+  {
+    unw_word_t val;
+#ifndef UNW_LOCAL_ONLY
+    unw_word_t type;            /* see DWARF_LOC_TYPE_* macros.  */
+#endif
+  }
+dwarf_loc_t;
+
+#endif /* dwarf_config_h */

commit 3ed9129de0c3dd71f7a61b1675ea805f4f6a002e

[LoongArch64] Fix build with upstream kernel headers
The libunwind LoongArch64 port was done using an early fork of Linux,
with slightly different naming for the register definition symbols.
The libunwind port went in before the kernel port got finalized and
merged, and was never adjusted; so fixing the usage here before release.
Practically no user would be affected since everyone on (development)
upstream kernels would have migrated long ago.

Fixes: c5f1d12c77de ("Add port for Linux on LoongArch")
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>
(cherry picked from commit 867885e7d7c1357bbd70a2e52060a1d730ce382a)

src/coredump/_UCD_access_reg_linux.c
@@ -70,37 +70,37 @@ _UCD_access_reg (unw_addr_space_t as,
 
   static const uint8_t remap_regs[] =
     {
-      [UNW_LOONGARCH64_R0]  = LOONGARCH64_EF_R0,
-      [UNW_LOONGARCH64_R1]  = LOONGARCH64_EF_R1,
-      [UNW_LOONGARCH64_R2]  = LOONGARCH64_EF_R2,
-      [UNW_LOONGARCH64_R3]  = LOONGARCH64_EF_R3,
-      [UNW_LOONGARCH64_R4]  = LOONGARCH64_EF_R4,
-      [UNW_LOONGARCH64_R5]  = LOONGARCH64_EF_R5,
-      [UNW_LOONGARCH64_R6]  = LOONGARCH64_EF_R6,
-      [UNW_LOONGARCH64_R7]  = LOONGARCH64_EF_R7,
-      [UNW_LOONGARCH64_R8]  = LOONGARCH64_EF_R8,
-      [UNW_LOONGARCH64_R9]  = LOONGARCH64_EF_R9,
-      [UNW_LOONGARCH64_R10] = LOONGARCH64_EF_R10,
-      [UNW_LOONGARCH64_R11] = LOONGARCH64_EF_R11,
-      [UNW_LOONGARCH64_R12] = LOONGARCH64_EF_R12,
-      [UNW_LOONGARCH64_R13] = LOONGARCH64_EF_R13,
-      [UNW_LOONGARCH64_R14] = LOONGARCH64_EF_R14,
-      [UNW_LOONGARCH64_R15] = LOONGARCH64_EF_R15,
-      [UNW_LOONGARCH64_R16] = LOONGARCH64_EF_R16,
-      [UNW_LOONGARCH64_R17] = LOONGARCH64_EF_R17,
-      [UNW_LOONGARCH64_R18] = LOONGARCH64_EF_R18,
-      [UNW_LOONGARCH64_R19] = LOONGARCH64_EF_R19,
-      [UNW_LOONGARCH64_R20] = LOONGARCH64_EF_R20,
-      [UNW_LOONGARCH64_R21] = LOONGARCH64_EF_R21,
-      [UNW_LOONGARCH64_R22] = LOONGARCH64_EF_R22,
-      [UNW_LOONGARCH64_R23] = LOONGARCH64_EF_R23,
-      [UNW_LOONGARCH64_R24] = LOONGARCH64_EF_R24,
-      [UNW_LOONGARCH64_R25] = LOONGARCH64_EF_R25,
-      [UNW_LOONGARCH64_R28] = LOONGARCH64_EF_R28,
-      [UNW_LOONGARCH64_R29] = LOONGARCH64_EF_R29,
-      [UNW_LOONGARCH64_R30] = LOONGARCH64_EF_R30,
-      [UNW_LOONGARCH64_R31] = LOONGARCH64_EF_R31,
-      [UNW_LOONGARCH64_PC]  = LOONGARCH64_EF_CSR_EPC,
+      [UNW_LOONGARCH64_R0]  = LOONGARCH_EF_R0,
+      [UNW_LOONGARCH64_R1]  = LOONGARCH_EF_R1,
+      [UNW_LOONGARCH64_R2]  = LOONGARCH_EF_R2,
+      [UNW_LOONGARCH64_R3]  = LOONGARCH_EF_R3,
+      [UNW_LOONGARCH64_R4]  = LOONGARCH_EF_R4,
+      [UNW_LOONGARCH64_R5]  = LOONGARCH_EF_R5,
+      [UNW_LOONGARCH64_R6]  = LOONGARCH_EF_R6,
+      [UNW_LOONGARCH64_R7]  = LOONGARCH_EF_R7,
+      [UNW_LOONGARCH64_R8]  = LOONGARCH_EF_R8,
+      [UNW_LOONGARCH64_R9]  = LOONGARCH_EF_R9,
+      [UNW_LOONGARCH64_R10] = LOONGARCH_EF_R10,
+      [UNW_LOONGARCH64_R11] = LOONGARCH_EF_R11,
+      [UNW_LOONGARCH64_R12] = LOONGARCH_EF_R12,
+      [UNW_LOONGARCH64_R13] = LOONGARCH_EF_R13,
+      [UNW_LOONGARCH64_R14] = LOONGARCH_EF_R14,
+      [UNW_LOONGARCH64_R15] = LOONGARCH_EF_R15,
+      [UNW_LOONGARCH64_R16] = LOONGARCH_EF_R16,
+      [UNW_LOONGARCH64_R17] = LOONGARCH_EF_R17,
+      [UNW_LOONGARCH64_R18] = LOONGARCH_EF_R18,
+      [UNW_LOONGARCH64_R19] = LOONGARCH_EF_R19,
+      [UNW_LOONGARCH64_R20] = LOONGARCH_EF_R20,
+      [UNW_LOONGARCH64_R21] = LOONGARCH_EF_R21,
+      [UNW_LOONGARCH64_R22] = LOONGARCH_EF_R22,
+      [UNW_LOONGARCH64_R23] = LOONGARCH_EF_R23,
+      [UNW_LOONGARCH64_R24] = LOONGARCH_EF_R24,
+      [UNW_LOONGARCH64_R25] = LOONGARCH_EF_R25,
+      [UNW_LOONGARCH64_R28] = LOONGARCH_EF_R28,
+      [UNW_LOONGARCH64_R29] = LOONGARCH_EF_R29,
+      [UNW_LOONGARCH64_R30] = LOONGARCH_EF_R30,
+      [UNW_LOONGARCH64_R31] = LOONGARCH_EF_R31,
+      [UNW_LOONGARCH64_PC]  = LOONGARCH_EF_CSR_ERA,
     };
 #else
 #if defined(UNW_TARGET_MIPS)

src/ptrace/_UPT_reg_offset.c
@@ -757,39 +757,39 @@ const int _UPT_reg_offset[UNW_REG_LAST + 1] =
 #elif defined(UNW_TARGET_LOONGARCH64)
 # include <asm/reg.h>
 
-    [UNW_LOONGARCH64_R0]  = LOONGARCH64_EF_R0,
-    [UNW_LOONGARCH64_R1]  = LOONGARCH64_EF_R1,
-    [UNW_LOONGARCH64_R2]  = LOONGARCH64_EF_R2,
-    [UNW_LOONGARCH64_R3]  = LOONGARCH64_EF_R3,
-    [UNW_LOONGARCH64_R4]  = LOONGARCH64_EF_R4,
-    [UNW_LOONGARCH64_R5]  = LOONGARCH64_EF_R5,
-    [UNW_LOONGARCH64_R6]  = LOONGARCH64_EF_R6,
-    [UNW_LOONGARCH64_R7]  = LOONGARCH64_EF_R7,
-    [UNW_LOONGARCH64_R8]  = LOONGARCH64_EF_R8,
-    [UNW_LOONGARCH64_R9]  = LOONGARCH64_EF_R9,
-    [UNW_LOONGARCH64_R10] = LOONGARCH64_EF_R10,
-    [UNW_LOONGARCH64_R11] = LOONGARCH64_EF_R11,
-    [UNW_LOONGARCH64_R12] = LOONGARCH64_EF_R12,
-    [UNW_LOONGARCH64_R13] = LOONGARCH64_EF_R13,
-    [UNW_LOONGARCH64_R14] = LOONGARCH64_EF_R14,
-    [UNW_LOONGARCH64_R15] = LOONGARCH64_EF_R15,
-    [UNW_LOONGARCH64_R16] = LOONGARCH64_EF_R16,
-    [UNW_LOONGARCH64_R17] = LOONGARCH64_EF_R17,
-    [UNW_LOONGARCH64_R18] = LOONGARCH64_EF_R18,
-    [UNW_LOONGARCH64_R19] = LOONGARCH64_EF_R19,
-    [UNW_LOONGARCH64_R20] = LOONGARCH64_EF_R20,
-    [UNW_LOONGARCH64_R21] = LOONGARCH64_EF_R21,
-    [UNW_LOONGARCH64_R22] = LOONGARCH64_EF_R22,
-    [UNW_LOONGARCH64_R23] = LOONGARCH64_EF_R23,
-    [UNW_LOONGARCH64_R24] = LOONGARCH64_EF_R24,
-    [UNW_LOONGARCH64_R25] = LOONGARCH64_EF_R25,
-    [UNW_LOONGARCH64_R26] = LOONGARCH64_EF_R26,
-    [UNW_LOONGARCH64_R27] = LOONGARCH64_EF_R27,
-    [UNW_LOONGARCH64_R28] = LOONGARCH64_EF_R28,
-    [UNW_LOONGARCH64_R29] = LOONGARCH64_EF_R29,
-    [UNW_LOONGARCH64_R30] = LOONGARCH64_EF_R30,
-    [UNW_LOONGARCH64_R31] = LOONGARCH64_EF_R31,
-    [UNW_LOONGARCH64_PC]  = LOONGARCH64_EF_CSR_EPC
+    [UNW_LOONGARCH64_R0]  = LOONGARCH_EF_R0,
+    [UNW_LOONGARCH64_R1]  = LOONGARCH_EF_R1,
+    [UNW_LOONGARCH64_R2]  = LOONGARCH_EF_R2,
+    [UNW_LOONGARCH64_R3]  = LOONGARCH_EF_R3,
+    [UNW_LOONGARCH64_R4]  = LOONGARCH_EF_R4,
+    [UNW_LOONGARCH64_R5]  = LOONGARCH_EF_R5,
+    [UNW_LOONGARCH64_R6]  = LOONGARCH_EF_R6,
+    [UNW_LOONGARCH64_R7]  = LOONGARCH_EF_R7,
+    [UNW_LOONGARCH64_R8]  = LOONGARCH_EF_R8,
+    [UNW_LOONGARCH64_R9]  = LOONGARCH_EF_R9,
+    [UNW_LOONGARCH64_R10] = LOONGARCH_EF_R10,
+    [UNW_LOONGARCH64_R11] = LOONGARCH_EF_R11,
+    [UNW_LOONGARCH64_R12] = LOONGARCH_EF_R12,
+    [UNW_LOONGARCH64_R13] = LOONGARCH_EF_R13,
+    [UNW_LOONGARCH64_R14] = LOONGARCH_EF_R14,
+    [UNW_LOONGARCH64_R15] = LOONGARCH_EF_R15,
+    [UNW_LOONGARCH64_R16] = LOONGARCH_EF_R16,
+    [UNW_LOONGARCH64_R17] = LOONGARCH_EF_R17,
+    [UNW_LOONGARCH64_R18] = LOONGARCH_EF_R18,
+    [UNW_LOONGARCH64_R19] = LOONGARCH_EF_R19,
+    [UNW_LOONGARCH64_R20] = LOONGARCH_EF_R20,
+    [UNW_LOONGARCH64_R21] = LOONGARCH_EF_R21,
+    [UNW_LOONGARCH64_R22] = LOONGARCH_EF_R22,
+    [UNW_LOONGARCH64_R23] = LOONGARCH_EF_R23,
+    [UNW_LOONGARCH64_R24] = LOONGARCH_EF_R24,
+    [UNW_LOONGARCH64_R25] = LOONGARCH_EF_R25,
+    [UNW_LOONGARCH64_R26] = LOONGARCH_EF_R26,
+    [UNW_LOONGARCH64_R27] = LOONGARCH_EF_R27,
+    [UNW_LOONGARCH64_R28] = LOONGARCH_EF_R28,
+    [UNW_LOONGARCH64_R29] = LOONGARCH_EF_R29,
+    [UNW_LOONGARCH64_R30] = LOONGARCH_EF_R30,
+    [UNW_LOONGARCH64_R31] = LOONGARCH_EF_R31,
+    [UNW_LOONGARCH64_PC]  = LOONGARCH_EF_CSR_ERA
 #else
 # error Fix me.
 #endif