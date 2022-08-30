xen0n/libunwind

commit c5f1d12c77dea6a60740730c675fc56b3c52b86a

Add port for Linux on LoongArch
This commit adds support for Linux on LoongArch. Only 64-bit is supported
at the moment.

The changes were tested on a LoongArch architecture Loongson-3A5000
processor (LoongArch is a new RISC ISA, which is a bit like MIPS or
RISC-V.).

Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
Signed-off-by: Qing Zhang <zhangqing@loongson.cn>
Signed-off-by: Jinyang He <hejinyang@loongson.cn>
Signed-off-by: Youling Tang <tangyouling@loongson.cn>

include/libunwind-loongarch64.h
@@ -1,0 +1,139 @@
+/* libunwind - a platform-independent unwind library
+   Copyright (C) 2001-2004 Hewlett-Packard Co
+        Contributed by David Mosberger-Tang <davidm@hpl.hp.com>
+   Copyright (C) 2021 Loongson Technology Corporation Limited
+
+This file is part of libunwind.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+
+#ifndef LIBUNWIND_H
+#define LIBUNWIND_H
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+#include <inttypes.h>
+#include <ucontext.h>
+
+#define UNW_TARGET      loongarch64
+#define UNW_TARGET_LOONGARCH64 1
+
+#define _U_TDEP_QP_TRUE 0       /* see libunwind-dynamic.h  */
+
+/* This needs to be big enough to accommodate "struct cursor", while
+   leaving some slack for future expansion.  Changing this value will
+   require recompiling all users of this library.  Stack allocation is
+   relatively cheap and unwind-state copying is relatively rare, so we
+   want to err on making it rather too big than too small.  */
+
+/* FIXME for LoongArch64. Too big?  What do other things use for similar tasks?  */
+#define UNW_TDEP_CURSOR_LEN     4096
+
+typedef uint64_t unw_word_t;
+typedef int64_t unw_sword_t;
+
+typedef long double unw_tdep_fpreg_t;
+
+typedef enum
+  {
+    UNW_LOONGARCH64_R0,
+    UNW_LOONGARCH64_R1,
+    UNW_LOONGARCH64_R2,
+    UNW_LOONGARCH64_R3,
+    UNW_LOONGARCH64_R4,
+    UNW_LOONGARCH64_R5,
+    UNW_LOONGARCH64_R6,
+    UNW_LOONGARCH64_R7,
+    UNW_LOONGARCH64_R8,
+    UNW_LOONGARCH64_R9,
+    UNW_LOONGARCH64_R10,
+    UNW_LOONGARCH64_R11,
+    UNW_LOONGARCH64_R12,
+    UNW_LOONGARCH64_R13,
+    UNW_LOONGARCH64_R14,
+    UNW_LOONGARCH64_R15,
+    UNW_LOONGARCH64_R16,
+    UNW_LOONGARCH64_R17,
+    UNW_LOONGARCH64_R18,
+    UNW_LOONGARCH64_R19,
+    UNW_LOONGARCH64_R20,
+    UNW_LOONGARCH64_R21,
+    UNW_LOONGARCH64_R22,
+    UNW_LOONGARCH64_R23,
+    UNW_LOONGARCH64_R24,
+    UNW_LOONGARCH64_R25,
+    UNW_LOONGARCH64_R26,
+    UNW_LOONGARCH64_R27,
+    UNW_LOONGARCH64_R28,
+    UNW_LOONGARCH64_R29,
+    UNW_LOONGARCH64_R30,
+    UNW_LOONGARCH64_R31,
+
+    UNW_LOONGARCH64_PC = 32,
+
+    /* FIXME: Other registers!  */
+
+    /* For LoongArch64, the CFA is the value of SP (r3) at the call site in the
+       previous frame.  */
+    UNW_LOONGARCH64_CFA,
+
+    UNW_TDEP_LAST_REG = UNW_LOONGARCH64_PC,
+
+    UNW_TDEP_IP = UNW_LOONGARCH64_R1,
+    UNW_TDEP_SP = UNW_LOONGARCH64_R3,
+    UNW_TDEP_EH = UNW_LOONGARCH64_R0   /* FIXME.  */
+  }
+loongarch64_regnum_t;
+
+#define UNW_TDEP_NUM_EH_REGS	4
+
+typedef struct unw_tdep_save_loc
+  {
+    /* Additional target-dependent info on a save location.  */
+    char unused;
+  }
+unw_tdep_save_loc_t;
+
+/* On LoongArch64, we can directly use ucontext_t as the unwind context. */
+typedef ucontext_t unw_tdep_context_t;
+
+typedef struct
+  {
+    /* no loongarch64-specific auxiliary proc-info */
+    char unused;
+  }
+unw_tdep_proc_info_t;
+
+#include "libunwind-dynamic.h"
+#include "libunwind-common.h"
+
+#define unw_tdep_getcontext		UNW_ARCH_OBJ(getcontext)
+extern int unw_tdep_getcontext (ucontext_t *uc);
+
+#define unw_tdep_is_fpreg		UNW_ARCH_OBJ(is_fpreg)
+extern int unw_tdep_is_fpreg (int);
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+
+#endif /* LIBUNWIND_H */

src/loongarch64/Gresume.c
@@ -1,0 +1,132 @@
+/* libunwind - a platform-independent unwind library
+   Copyright (C) 2008 CodeSourcery
+   Copyright (C) 2021 Loongson Technology Corporation Limited
+
+This file is part of libunwind.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+
+/* FIXME for LoongArch64.  */
+
+#include <stdlib.h>
+
+#include "unwind_i.h"
+
+#ifndef UNW_REMOTE_ONLY
+
+HIDDEN inline int
+loongarch64_local_resume (unw_addr_space_t as, unw_cursor_t *cursor, void *arg)
+{
+  struct cursor *c = (struct cursor *) cursor;
+  unw_tdep_context_t *uc = c->uc;
+
+  if (c->sigcontext_format == LOONGARCH64_SCF_NONE)
+    {
+      /* Since there are no signals involved here we restore EH and non scratch
+         registers only.  */
+      unsigned long sp = uc->uc_mcontext.__gregs[3];
+      unsigned long ra = uc->uc_mcontext.__gregs[1];
+
+      asm volatile (
+        "move $t0, %0\n"
+        "move $t1, %1\n"
+        "move $t2, %2\n"
+        "ld.d $fp, $t0, 22*8\n"
+        "ld.d $s0, $t0, 23*8\n"
+        "ld.d $s1, $t0, 24*8\n"
+        "ld.d $s2, $t0, 25*8\n"
+        "ld.d $s3, $t0, 26*8\n"
+        "ld.d $s4, $t0, 27*8\n"
+        "ld.d $s5, $t0, 28*8\n"
+        "ld.d $s6, $t0, 29*8\n"
+        "ld.d $s7, $t0, 30*8\n"
+        "ld.d $s8, $t0, 31*8\n"
+        "move $ra, $t2\n"
+        "move $sp, $t1\n"
+        "jirl $r0, $ra, 0\n"
+        :
+        : "r" (uc->uc_mcontext.__gregs),
+          "r" (sp),
+          "r" (ra)
+      );
+    }
+  else /* c->sigcontext_format == LOONGARCH64_SCF_LINUX_RT_SIGFRAME */
+    {
+      int i;
+      struct sigcontext *sc = (struct sigcontext *) c->sigcontext_addr;
+
+      sc->sc_pc = c->dwarf.ip;
+      for (i = UNW_LOONGARCH64_R0; i <= UNW_LOONGARCH64_R31; i++)
+            sc->sc_regs[i] = uc->uc_mcontext.__gregs[i];
+
+      Debug (8, "resuming at ip=0x%lx via sigreturn() (trampoline @ 0x%lx, sp @ 0x%lx)\n",
+        c->dwarf.ip, c->sigcontext_pc, c->sigcontext_sp);
+
+      asm volatile (
+        "move $sp, %0\n"
+        "jirl $r0, %1, 0\n"
+        : : "r" (c->sigcontext_sp), "r" (c->sigcontext_pc)
+      );
+   }
+  unreachable();
+
+  return -UNW_EINVAL;
+}
+
+#endif /* !UNW_REMOTE_ONLY */
+
+static inline void
+establish_machine_state (struct cursor *c)
+{
+  unw_addr_space_t as = c->dwarf.as;
+  void *arg = c->dwarf.as_arg;
+  unw_word_t val;
+  int reg;
+
+  Debug (8, "copying out cursor state\n");
+
+  for (reg = UNW_LOONGARCH64_R0; reg <= UNW_LOONGARCH64_R31; reg++)
+    {
+      Debug (16, "copying %s %d\n", unw_regname (reg), reg);
+      if (tdep_access_reg (c, reg, &val, 0) >= 0)
+        as->acc.access_reg (as, reg, &val, 1, arg);
+    }
+}
+
+int
+unw_resume (unw_cursor_t *cursor)
+{
+  struct cursor *c = (struct cursor *) cursor;
+
+  Debug (1, "(cursor=%p)\n", c);
+
+  if (!c->dwarf.ip)
+    {
+      /* This can happen easily when the frame-chain gets truncated
+         due to bad or missing unwind-info.  */
+      Debug (1, "refusing to resume execution at address 0\n");
+      return -UNW_EINVAL;
+    }
+
+  establish_machine_state (c);
+
+  return (*c->dwarf.as->acc.resume) (c->dwarf.as, (unw_cursor_t *) c,
+                                     c->dwarf.as_arg);
+}

src/loongarch64/Gstep.c
@@ -1,0 +1,145 @@
+/* libunwind - a platform-independent unwind library
+   Copyright (C) 2008 CodeSourcery
+   Copyright (C) 2015 Imagination Technologies Limited
+   Copyright (C) 2021 Loongson Technology Corporation Limited
+
+This file is part of libunwind.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
+
+#include "unwind_i.h"
+#include "offsets.h"
+
+static int
+loongarch64_handle_signal_frame (unw_cursor_t *cursor)
+{
+  struct cursor *c = (struct cursor *) cursor;
+  unw_word_t sc_addr, sp_addr = c->dwarf.cfa;
+  unw_word_t ra, fp;
+  int ret;
+
+  if (unw_is_signal_frame (cursor)) {
+    sc_addr = sp_addr + LINUX_SF_TRAMP_SIZE + sizeof (siginfo_t) +
+              LINUX_UC_MCONTEXT_OFF;
+  } else {
+    c->sigcontext_format = LOONGARCH64_SCF_NONE;
+    return -UNW_EUNSPEC;
+  }
+
+  c->sigcontext_addr = sc_addr;
+
+  /* Save the SP and PC to be able to return execution at this point
+    later in time (unw_resume).  */
+  c->sigcontext_sp = c->dwarf.cfa;
+  c->sigcontext_pc = c->dwarf.ip;
+  c->sigcontext_format = LOONGARCH64_SCF_LINUX_RT_SIGFRAME;
+
+    /* Update the dwarf cursor.
+     Set the location of the registers to the corresponding addresses of the
+     uc_mcontext / sigcontext structure contents.  */
+
+  c->dwarf.loc[UNW_LOONGARCH64_R0]  = DWARF_LOC (sc_addr + LINUX_SC_R0_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R1]  = DWARF_LOC (sc_addr + LINUX_SC_R1_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R2]  = DWARF_LOC (sc_addr + LINUX_SC_R2_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R3]  = DWARF_LOC (sc_addr + LINUX_SC_R3_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R4]  = DWARF_LOC (sc_addr + LINUX_SC_R4_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R5]  = DWARF_LOC (sc_addr + LINUX_SC_R5_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R6]  = DWARF_LOC (sc_addr + LINUX_SC_R6_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R7]  = DWARF_LOC (sc_addr + LINUX_SC_R7_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R8]  = DWARF_LOC (sc_addr + LINUX_SC_R8_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R9]  = DWARF_LOC (sc_addr + LINUX_SC_R9_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R10] = DWARF_LOC (sc_addr + LINUX_SC_R10_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R11] = DWARF_LOC (sc_addr + LINUX_SC_R11_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R12] = DWARF_LOC (sc_addr + LINUX_SC_R12_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R13] = DWARF_LOC (sc_addr + LINUX_SC_R13_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R14] = DWARF_LOC (sc_addr + LINUX_SC_R14_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R15] = DWARF_LOC (sc_addr + LINUX_SC_R15_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R16] = DWARF_LOC (sc_addr + LINUX_SC_R16_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R17] = DWARF_LOC (sc_addr + LINUX_SC_R17_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R18] = DWARF_LOC (sc_addr + LINUX_SC_R18_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R19] = DWARF_LOC (sc_addr + LINUX_SC_R19_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R20] = DWARF_LOC (sc_addr + LINUX_SC_R20_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R21] = DWARF_LOC (sc_addr + LINUX_SC_R21_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R22] = DWARF_LOC (sc_addr + LINUX_SC_R22_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R23] = DWARF_LOC (sc_addr + LINUX_SC_R23_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R24] = DWARF_LOC (sc_addr + LINUX_SC_R24_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R25] = DWARF_LOC (sc_addr + LINUX_SC_R25_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R26] = DWARF_LOC (sc_addr + LINUX_SC_R26_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R27] = DWARF_LOC (sc_addr + LINUX_SC_R27_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R28] = DWARF_LOC (sc_addr + LINUX_SC_R28_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R29] = DWARF_LOC (sc_addr + LINUX_SC_R29_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R30] = DWARF_LOC (sc_addr + LINUX_SC_R30_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_R31] = DWARF_LOC (sc_addr + LINUX_SC_R31_OFF, 0);
+  c->dwarf.loc[UNW_LOONGARCH64_PC] = DWARF_LOC (sc_addr + LINUX_SC_PC_OFF, 0);
+
+  /* Set SP/CFA and PC/IP. */
+  dwarf_get (&c->dwarf, c->dwarf.loc[UNW_LOONGARCH64_R3], &c->dwarf.cfa);
+
+  if ((ret = dwarf_get(&c->dwarf, DWARF_LOC(sc_addr + LINUX_SC_PC_OFF, 0),
+                       &c->dwarf.ip)) < 0)
+    return ret;
+
+  if ((ret = dwarf_get(&c->dwarf, DWARF_LOC(sc_addr + LINUX_SC_R1_OFF, 0),
+                       &ra)) < 0)
+    return ret;
+  if ((ret = dwarf_get(&c->dwarf, DWARF_LOC(sc_addr + LINUX_SC_R22_OFF, 0),
+                       &fp)) < 0)
+    return ret;
+
+  Debug (2, "SH (ip=0x%016llx, ra=0x%016llx, sp=0x%016llx, fp=0x%016llx)\n",
+         (unsigned long long)c->dwarf.ip, (unsigned long long)ra,
+         (unsigned long long)c->dwarf.cfa, (unsigned long long)fp);
+
+  c->dwarf.pi_valid = 0;
+  c->dwarf.use_prev_instr = 0;
+
+  return 1;
+
+}
+
+int
+unw_step (unw_cursor_t *cursor)
+{
+  struct cursor *c = (struct cursor *) cursor;
+  int validate = c->validate;
+  int ret;
+
+  Debug (1, "(cursor=%p, ip=0x%016lx, sp=0x%016lx)\n",
+         c, c->dwarf.ip, c->dwarf.cfa);
+
+  ret = unw_is_signal_frame (cursor);
+  if (ret > 0)
+    return loongarch64_handle_signal_frame (cursor);
+
+  /* Not a signal frame, try DWARF-based unwinding. */
+  ret = dwarf_step (&c->dwarf);
+
+  /* Restore default memory validation state */
+  c->validate = validate;
+
+  if (unlikely (ret == -UNW_ESTOPUNWIND))
+    return ret;
+
+  /* Dwarf unwinding didn't work, stop.  */
+  if (unlikely (ret < 0))
+    return 0;
+
+  return (c->dwarf.ip == 0) ? 0 : 1;
+}

src/loongarch64/offsets.h
@@ -1,0 +1,54 @@
+/* Linux-specific definitions: */
+
+/* Define various structure offsets to simplify cross-compilation.  */
+
+/* Offsets for LoongArch64 Linux "ucontext_t":  */
+
+/* First 24 bytes in sigframe are argument save space and padding for
+what used to be signal trampolines. Ref: arch/loongarch/kernel/signal.c */
+#define LINUX_SF_TRAMP_SIZE 0x18
+
+# define LINUX_UC_FLAGS_OFF		0x0	/* offsetof(struct ucontext_t, __uc_flags) */
+# define LINUX_UC_LINK_OFF		0x8	/* offsetof(struct ucontext_t, uc_link) */
+# define LINUX_UC_STACK_OFF		0x10	/* offsetof(struct ucontext_t, uc_stack) */
+# define LINUX_UC_SIGMASK_OFF		0x28	/* offsetof(struct ucontext_t, uc_sigmask) */
+# define LINUX_UC_MCONTEXT_OFF		0xc0	/* offsetof(struct ucontext_t, uc_mcontext) */
+
+# define LINUX_UC_MCONTEXT_PC		0xc0	/* offsetof(struct ucontext_t, uc_mcontext.__pc) */
+# define LINUX_UC_MCONTEXT_GREGS	0xc8	/* offsetof(struct ucontext_t, uc_mcontext.__gregs) */
+/* Offsets for AArch64 Linux "struct sigcontext": */
+#define LINUX_SC_R0_OFF   (LINUX_UC_MCONTEXT_GREGS - LINUX_UC_MCONTEXT_OFF)
+#define LINUX_SC_R1_OFF   (LINUX_SC_R0_OFF + 1*8)
+#define LINUX_SC_R2_OFF   (LINUX_SC_R0_OFF + 2*8)
+#define LINUX_SC_R3_OFF   (LINUX_SC_R0_OFF + 3*8)
+#define LINUX_SC_R4_OFF   (LINUX_SC_R0_OFF + 4*8)
+#define LINUX_SC_R5_OFF   (LINUX_SC_R0_OFF + 5*8)
+#define LINUX_SC_R6_OFF   (LINUX_SC_R0_OFF + 6*8)
+#define LINUX_SC_R7_OFF   (LINUX_SC_R0_OFF + 7*8)
+#define LINUX_SC_R8_OFF   (LINUX_SC_R0_OFF + 8*8)
+#define LINUX_SC_R9_OFF   (LINUX_SC_R0_OFF + 9*8)
+#define LINUX_SC_R10_OFF  (LINUX_SC_R0_OFF + 10*8)
+#define LINUX_SC_R11_OFF  (LINUX_SC_R0_OFF + 11*8)
+#define LINUX_SC_R12_OFF  (LINUX_SC_R0_OFF + 12*8)
+#define LINUX_SC_R13_OFF  (LINUX_SC_R0_OFF + 13*8)
+#define LINUX_SC_R14_OFF  (LINUX_SC_R0_OFF + 14*8)
+#define LINUX_SC_R15_OFF  (LINUX_SC_R0_OFF + 15*8)
+#define LINUX_SC_R16_OFF  (LINUX_SC_R0_OFF + 16*8)
+#define LINUX_SC_R17_OFF  (LINUX_SC_R0_OFF + 17*8)
+#define LINUX_SC_R18_OFF  (LINUX_SC_R0_OFF + 18*8)
+#define LINUX_SC_R19_OFF  (LINUX_SC_R0_OFF + 19*8)
+#define LINUX_SC_R20_OFF  (LINUX_SC_R0_OFF + 20*8)
+#define LINUX_SC_R21_OFF  (LINUX_SC_R0_OFF + 21*8)
+#define LINUX_SC_R22_OFF  (LINUX_SC_R0_OFF + 22*8)
+#define LINUX_SC_R23_OFF  (LINUX_SC_R0_OFF + 23*8)
+#define LINUX_SC_R24_OFF  (LINUX_SC_R0_OFF + 24*8)
+#define LINUX_SC_R25_OFF  (LINUX_SC_R0_OFF + 25*8)
+#define LINUX_SC_R26_OFF  (LINUX_SC_R0_OFF + 26*8)
+#define LINUX_SC_R27_OFF  (LINUX_SC_R0_OFF + 27*8)
+#define LINUX_SC_R28_OFF  (LINUX_SC_R0_OFF + 28*8)
+#define LINUX_SC_R29_OFF  (LINUX_SC_R0_OFF + 29*8)
+#define LINUX_SC_R30_OFF  (LINUX_SC_R0_OFF + 30*8)
+#define LINUX_SC_R31_OFF  (LINUX_SC_R0_OFF + 31*8)
+
+#define LINUX_SC_SP_OFF   LINUX_SC_R3_OFF
+#define LINUX_SC_PC_OFF   (LINUX_UC_MCONTEXT_PC - LINUX_UC_MCONTEXT_OFF)

Makefile.am
@@ -47,6 +47,9 @@ endif
 if ARCH_RISCV
 include_HEADERS += include/libunwind-riscv.h
 endif
+if ARCH_LOONGARCH64
+include_HEADERS += include/libunwind-loongarch64.h
+endif
 
 if !REMOTE_ONLY
 include_HEADERS += include/libunwind.h
@@ -97,6 +100,9 @@ noinst_HEADERS = include/dwarf.h include/dwarf_i.h include/dwarf-eh.h	\
 	include/tdep-s390x/jmpbuf.h include/tdep-s390x/libunwind_i.h	\
 	include/tdep-riscv/dwarf-config.h				\
 	include/tdep-riscv/jmpbuf.h include/tdep-riscv/libunwind_i.h	\
+	include/tdep-loongarch64/dwarf-config.h				\
+	include/tdep-loongarch64/jmpbuf.h				\
+	include/tdep-loongarch64/libunwind_i.h				\
 	include/tdep/libunwind_i.h					\
 	include/tdep/jmpbuf.h include/tdep/dwarf-config.h
 

README
@@ -18,6 +18,7 @@ This library supports several architecture/operating-system combinations:
 | Linux   | Tilegx       | 64-bit mode only |
 | Linux   | MIPS         | Newly added |
 | Linux   | RISC-V       | 64-bit only |
+| Linux   | LoongArch    | 64-bit only |
 | HP-UX   | IA-64        | Mostly works, but known to have serious limitations |
 | FreeBSD | x86-64       | ✓      |
 | FreeBSD | x86          | ✓      |
@@ -41,6 +42,7 @@ such dependencies
 |    arm       |     p      |            |
 |    hppa      |     p      |      p     |
 |    ia64      |     p      |      r     |
+|    loongarch |     p      |            |
 |    mips      |     p      |            |
 |    ppc32     |     r      |            |
 |    ppc64     |     r      |      r     |

configure.ac
@@ -98,6 +98,7 @@ AC_DEFUN([SET_ARCH],[
         [amd64],[$2=x86_64],
         [tile*],[$2=tilegx],
         [riscv*],[$2=riscv],
+        [loongarch64*],[$2=loongarch64],
         [$2=$1])
 ]) dnl SET_ARCH
 
@@ -120,7 +121,7 @@ esac
 
 AC_ARG_ENABLE(coredump,
 	AS_HELP_STRING([--enable-coredump],[building libunwind-coredump library]),,
-        [AS_CASE([$host_arch], [aarch64*|arm*|mips*|sh*|x86*|tile*|riscv*], [enable_coredump=yes], [enable_coredump=no])]
+        [AS_CASE([$host_arch], [aarch64*|arm*|mips*|sh*|x86*|tile*|riscv*|loongarch64], [enable_coredump=yes], [enable_coredump=no])]
 )
 
 AC_MSG_CHECKING([if we should build libunwind-coredump])
@@ -189,6 +190,7 @@ AM_CONDITIONAL(ARCH_SH, test x$target_arch = xsh)
 AM_CONDITIONAL(ARCH_TILEGX, test x$target_arch = xtilegx)
 AM_CONDITIONAL(ARCH_S390X, test x$target_arch = xs390x)
 AM_CONDITIONAL(ARCH_RISCV, test x$target_arch = xriscv)
+AM_CONDITIONAL(ARCH_LOONGARCH64, test x$target_arch = xloongarch64)
 AM_CONDITIONAL(OS_LINUX, expr x$target_os : xlinux >/dev/null)
 AM_CONDITIONAL(OS_HPUX, expr x$target_os : xhpux >/dev/null)
 AM_CONDITIONAL(OS_FREEBSD, expr x$target_os : xfreebsd >/dev/null)
@@ -199,7 +201,7 @@ AC_MSG_CHECKING([for ELF helper width])
 case "${target_arch}" in
 (arm|hppa|ppc32|x86|sh) use_elf32=yes; AC_MSG_RESULT([32]);;
 (aarch64|ia64|ppc64|x86_64|s390x|tilegx)  use_elf64=yes; AC_MSG_RESULT([64]);;
-(mips|riscv)                 use_elfxx=yes; AC_MSG_RESULT([xx]);;
+(mips|riscv|loongarch64)                  use_elfxx=yes; AC_MSG_RESULT([xx]);;
 *)                     AC_MSG_ERROR([Unknown ELF target: ${target_arch}])
 esac
 AM_CONDITIONAL(USE_ELF32, [test x$use_elf32 = xyes])
@@ -246,6 +248,7 @@ case $target_arch in
   mips*) enable_cxx_exceptions=no;;
   tile*) enable_cxx_exceptions=no;;
   s390x*) enable_cxx_exceptions=no;;
+  loongarch*) enable_cxx_exceptions=no;;
   *) enable_cxx_exceptions=yes;;
 esac
 ])

include/libunwind.h.in
@@ -29,6 +29,8 @@
 # include "libunwind-s390x.h"
 #elif defined __riscv || defined __riscv__
 # include "libunwind-riscv.h"
+#elif defined __loongarch64
+# include "libunwind-loongarch64.h"
 #else
 # error "Unsupported arch"
 #endif

commit e63d87a930c4034db40ca84c7d7bf4dfb63497bc

[LoongArch64] Reflect the kernel ABI changes since the port went upstream
The original port was done with an early in-house port of Linux that, in
addition to slightly different UAPI headers, also featured a MIPS-like
ABI (the so-called "old world" ABI). The upstream ABI has been revised
since long ago and already frozen, so adjust the port for the "new world".

We don't need to keep compatibility for the old-world kernels, because
distributions using said kernels invariably packaged their own libunwind
fork with corresponding support, and the few users tracking upstream
kernels should all have moved on.

Fixes: c5f1d12c77de ("Add port for Linux on LoongArch")
Signed-off-by: Youling Tang <tangyouling@loongson.cn>
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>
(cherry picked from commit bdc00ea91ade817501a309647287bc028197beef)

include/libunwind-loongarch64.h
@@ -88,7 +88,7 @@ typedef enum
     UNW_LOONGARCH64_R30,
     UNW_LOONGARCH64_R31,
 
-    UNW_LOONGARCH64_PC = 32,
+    UNW_LOONGARCH64_PC = 33,
 
     /* FIXME: Other registers!  */
 

src/loongarch64/Gresume.c
@@ -65,6 +65,7 @@ loongarch64_local_resume (unw_addr_space_t as, unw_cursor_t *cursor, void *arg)
         : "r" (uc->uc_mcontext.__gregs),
           "r" (sp),
           "r" (ra)
+        : "$t0", "$t1", "$t2", "memory"
       );
     }
   else /* c->sigcontext_format == LOONGARCH64_SCF_LINUX_RT_SIGFRAME */

src/loongarch64/Gstep.c
@@ -36,8 +36,7 @@ loongarch64_handle_signal_frame (unw_cursor_t *cursor)
   int ret;
 
   if (unw_is_signal_frame (cursor)) {
-    sc_addr = sp_addr + LINUX_SF_TRAMP_SIZE + sizeof (siginfo_t) +
-              LINUX_UC_MCONTEXT_OFF;
+    sc_addr = sp_addr + sizeof (siginfo_t) + LINUX_UC_MCONTEXT_OFF;
   } else {
     c->sigcontext_format = LOONGARCH64_SCF_NONE;
     return -UNW_EUNSPEC;

src/loongarch64/offsets.h
@@ -4,19 +4,15 @@
 
 /* Offsets for LoongArch64 Linux "ucontext_t":  */
 
-/* First 24 bytes in sigframe are argument save space and padding for
-what used to be signal trampolines. Ref: arch/loongarch/kernel/signal.c */
-#define LINUX_SF_TRAMP_SIZE 0x18
-
 # define LINUX_UC_FLAGS_OFF		0x0	/* offsetof(struct ucontext_t, __uc_flags) */
 # define LINUX_UC_LINK_OFF		0x8	/* offsetof(struct ucontext_t, uc_link) */
 # define LINUX_UC_STACK_OFF		0x10	/* offsetof(struct ucontext_t, uc_stack) */
 # define LINUX_UC_SIGMASK_OFF		0x28	/* offsetof(struct ucontext_t, uc_sigmask) */
-# define LINUX_UC_MCONTEXT_OFF		0xc0	/* offsetof(struct ucontext_t, uc_mcontext) */
+# define LINUX_UC_MCONTEXT_OFF		0xb0	/* offsetof(struct ucontext_t, uc_mcontext) */
 
-# define LINUX_UC_MCONTEXT_PC		0xc0	/* offsetof(struct ucontext_t, uc_mcontext.__pc) */
-# define LINUX_UC_MCONTEXT_GREGS	0xc8	/* offsetof(struct ucontext_t, uc_mcontext.__gregs) */
-/* Offsets for AArch64 Linux "struct sigcontext": */
+# define LINUX_UC_MCONTEXT_PC		0xb0	/* offsetof(struct ucontext_t, uc_mcontext.__pc) */
+# define LINUX_UC_MCONTEXT_GREGS	0xb8	/* offsetof(struct ucontext_t, uc_mcontext.__gregs) */
+/* Offsets for LoongArch64 Linux "struct sigcontext": */
 #define LINUX_SC_R0_OFF   (LINUX_UC_MCONTEXT_GREGS - LINUX_UC_MCONTEXT_OFF)
 #define LINUX_SC_R1_OFF   (LINUX_SC_R0_OFF + 1*8)
 #define LINUX_SC_R2_OFF   (LINUX_SC_R0_OFF + 2*8)